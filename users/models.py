f0438676db0a8412492e59fd66f04dfc2111ace2
blair <blairj0603@gmail.com>
2024-02-15 12:05:28 +0900

    회원가입 로그인 만들고 - 리뷰와 합치기위해 커밋

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(models.Model):
     user_id = models.CharField("사용자 아이디", unique = True, max_length = 16)
-    
-
+    password = models.CharField("비밀번호", unique = True, max_length = 20)
+    name = models.CharField("이름", unique = True, max_length = 20)
+    user_tel = models.CharField("전화번호", unique = False, max_length = 11)
+    email = models.CharField("이메일", unique = False, max_length =20)   
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from users.forms import LoginForm, SignupForm
+from django.contrib.auth import authenticate, login, logout
+from users.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+    return redirect("reviews/feeds_list")
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'users/login.html', {'error' : '회원명이나 비밀번호가 틀립니다'})
+        
+    return render(request, 'users/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('/')
+    return render(request, 'users/login.html')
+
+
+
